fa29a09f181330ead151762d4c98f7a94366d535
Servio Tulio Reyes Castillo <servio_trc@yahoo.com.mx>
2019-05-14 23:11:54 -0500

    Parentheses

--- Arcade/Intro/Smooth Sailing/reverseInParentheses.py
+++ Arcade/Intro/Smooth Sailing/reverseInParentheses.py
@@ -1,4 +1,30 @@
-def reverse_inside_parentheses(value):
+def reverse_inside_parentheses(word):
+    stack = []
+    last = len(word)
+    for index, letter in enumerate(word):
+        if letter == "(":
+            stack.append(letter)
+        elif stack and letter == ")":
+            stack.pop()
+        if len(stack) == 0 and index - 1 >= 0:
+            last = index
+            break
+    result = list(word[1:last])
+    result.reverse()
+    for index, letter in enumerate(result):
+        if letter == "(":
+            result[index] = ")"
+        elif letter == ")":
+            result[index] = "("
+    # print("salida", result)
+    return ''.join(result) + word[last+1:]
+        
 
-
-# def reverseInParentheses(inputString):
+def reverseInParentheses(inputString):
+    while True:
+        val = inputString.find("(")
+        if val <= -1:
+            break
+        previo = inputString[:val]
+        inputString = previo + reverse_inside_parentheses(inputString[val:])
+    return inputString
